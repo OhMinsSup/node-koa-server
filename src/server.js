0c1696d635d699921e19464762eac032158eff38
Your Name <you@example.com>
2019-03-29 06:32:18 +0900

    frist commit

---
+++ src/server.js
@@ -0,0 +1,34 @@
+require("dotenv").load();
+const Koa = require("koa");
+const mongoose = require("mongoose");
+const bodyParser = require("koa-bodyparser");
+const cors = require("./lib/cors");
+const tokenParser = require("./lib/tokenParser");
+
+const router = require("./router");
+
+const { PORT: port } = process.env;
+
+// MongoDB 연결
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Connected to mongodb");
+  })
+  .catch(e => {
+    console.log(e);
+  });
+mongoose.set("debug", true);
+
+const app = new Koa();
+
+app.use(cors);
+app.use(tokenParser);
+app.use(bodyParser());
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.proxy = true;
+
+app.listen(port || 8080, () => {
+  console.log("Server is listening to port %d", port || 8080);
+});
